e5ebddc28e48815010f0d3a3bc8b605cfb5ad2d0
alexbmt84 <alexisbeaumont97@gmail.com>
2023-09-16 13:57:19 +0200

    Modifs BobController

--- app/Http/Controllers/BobController.php
+++ app/Http/Controllers/BobController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Bob;
-class BobController extends Controller
-{
-    public function index()
-    {
+class BobController extends Controller {
+    public function index() {
+
         return view('home');
+
     }
 
     public function respond(Request $request) {
 
         $bob = new Bob();
+
         $reply = $bob->respondTo($request->input('message'));
+
         return view('home', ['reply' => $reply]);
 
     }
